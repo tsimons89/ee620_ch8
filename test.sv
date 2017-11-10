24d81a03f2c9e5e6897cb1fe743df2ca79e6ab28
Taylor <tsimons@ssh3.et.byu.edu>
2017-11-10 15:41:44 -0700

    Fixed test.sv

--- test.sv
+++ test.sv
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import TestRegestry_pkg::*;
+import TestRegistry_pkg::*;
 
 program automatic test;
    TestBase tb;
@@ -8,6 +7,3 @@ program automatic test;
       tb.run_test();
    end
 endprogram
-=======
-
->>>>>>> b44d08880321384c029e62905bfbbde93a7828b7
